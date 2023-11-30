d1ebdf0559014997d3348a1f5d4a6d85e3135008
tommmmaso <tommaso.poggi@zhdk.ch>
2023-11-30 16:53:22 +0100

    1

--- projects/01MainAssignments/project1/script.js
+++ projects/01MainAssignments/project1/script.js
@@ -1,29 +1,99 @@
-let len = 10;
-let bg;
+let gridSize = 3; // Dimensione della griglia
+let grid = []; // Array per la griglia
+let cellSize; // Dimensione della cella
+let Helv;
+let P22;
+let Maz;
+let Voy;
+let letter = "g";
+
+function preload() {
+
+  Helv = loadFont('assets/HelveticaNeueLTStd-Md copia.otf'); // Sostituisci con il tuo percorso e nome del file per il carattere Helvetica
+  P22 = loadFont('assets/DomaineDisplay-Semibold copia.otf'); // Sostituisci con il tuo percorso e nome del file per il carattere Garamond
+  Maz = loadFont('assets/MaziusDisplay-Bold copia.otf');
+  Voy = loadFont('assets/voyage-bold copia.otf');
+}
 
 function setup() {
-	createCanvas(windowWidth,windowHeight);
-	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(10, 200, 58);
+  createCanvas(756, 657);
+  generateGrid(); // Genera la griglia al setup
+  cellSize = width / gridSize; // Imposta la dimensione della cella in base alla griglia
+  textAlign(CENTER, CENTER);
+  noLoop();
 }
 
-function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(20,250, 40);
-	
-	rect(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-	rect(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(89, 30, 33);
-	}
+function draw() {
+  background("#111010"); // Sfondo bianco
+
+  // Itera sulla griglia
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      let x = width / 2 + (i - gridSize / 2) * cellSize; // Calcola la coordinata x del rettangolo rispetto al centro della tavola
+      let y = height / 2 + (j - gridSize / 2) * cellSize; // Calcola la coordinata y del rettangolo rispetto al centro della tavola
+
+      // Applica la maschera
+      drawingContext.save();
+      rect(x, y, cellSize, cellSize);
+      drawingContext.clip();
+
+      // Disegna il rettangolo della cella
+      noStroke();
+      fill("#111010");
+      rect(x, y, cellSize, cellSize);
+
+// Controlla cosa deve apparire dentro ogni sezione
+      let selectedFont;
+      	if (random() > 0.5) {
+        	selectedFont = random() > 0.5 ? Helv : P22;
+      	} else {
+        	selectedFont = random() > 0.5 ? Maz : Voy;
+      }
+
+      textFont(selectedFont);
+      textAlign(CENTER, CENTER);
+      textSize(750); // Cambia 70 con la grandezza desiderata
+      fill("#fafafa");
+
+      if (selectedFont === Maz) {
+// Se il font è "Maz", alza il punto di 170px
+      text(letter, width / 2, height / 2 - 230);
+			} else if (selectedFont === Voy) {
+// Se il font è "Voy", alza il punto di 220px
+ 			text(letter, width / 2, height / 2 - 280);
+			} else if (selectedFont === P22) {
+// Se il font è "P22", alza il punto di 220px
+ 	    text(letter, width / 2, height / 2 - 281.7);
+			} else {
+// La posizione standard
+      text(letter, width / 2, height / 2 - 160);
+      }
+			
+
+      // Ripristina il contesto di disegno
+      drawingContext.restore();
+    }
+  }
 }
 
+function generateGrid() {
+  // Genera la griglia 3x3 con valori casuali tra 1 e 2
+  for (let i = 0; i < gridSize; i++) {
+    grid[i] = [];
+    for (let j = 0; j < gridSize; j++) {
+      // Assegna un valore casuale tra 1 e 2 a ogni cella
+      grid[i][j] = int(random(1, 3));
+    }
+  }
+}
+
+function keyPressed() {
+  // Inverti i valori di tutte le celle
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      grid[i][j] = grid[i][j] === 1 ? 2 : 1;
+    }
+  }
+
+  redraw(); // Richiama la funzione draw per visualizzare le modifiche
+}
--- projects/01MainAssignments/project3/script.js
+++ projects/01MainAssignments/project3/script.js
@@ -1,29 +1,113 @@
-let len = 10;
-let bg;
+let pos = [];
+let img;
+const segNum = 10;
+const segLength = 40;
+let vol = 0;
+let volRadius = 20;
+let font;
+let mySound;
 
-function setup() {
-	createCanvas(windowWidth,windowHeight);
-	background(0);
-	frameRate(30);
-	rectMode(CENTER);
-	bg = color(200, 0, 58);
+function preload() {
+	img = loadImage('assets/VolumeIcon.png');
+  font = loadFont('assets/HelveticaNeueLTStd-HvCn.otf');
+	soundFormats('mp3', 'ogg');
+  mySound = loadSound('assets/CupHeadSound.mp3');
 }
 
-function  draw() {
-	background(bg);
-	noFill();
-	strokeWeight(5);
-	stroke(0,130, 140);
-	
-	ellipse(pmouseX, pmouseY, len, len)
-  //height - mouseY mirrors the object along y axis
-  	ellipse(pmouseX, height - pmouseY, len, len)
-	//increase the lenght by 1px every frame
-	len++;
-	
-	if(mouseIsPressed) {
-		len = 0;  
-		bg = color(189, 3, 33);
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	stroke("#FFD6A1");
+	strokeWeight(20);
+
+	img.loadPixels(); // Carica i pixel dell'immagine una sola volta
+
+	for (let i = 0; i < segNum; i++) {
+		pos.push({
+			x: 0,
+			y: 0
+		});
 	}
 }
 
+function draw() {
+	//background("#1271FF");
+	background("#FFAF12");
+
+  // Mappatura del volume	
+	let volumeMapped = map(vol, 0, 100, 0, 1);
+  mySound.setVolume(volumeMapped);
+
+	let parent = {
+		x: mouseX,
+		y: mouseY
+	};
+	let index = 10;
+	let hitCheck = false;
+let angle = 0;
+	for (let child of pos) {
+		angle = atan2(child.y - parent.y, child.x - parent.x);
+		child.x = parent.x + cos(angle) * segLength;
+		child.y = parent.y + sin(angle) * segLength;
+		line(parent.x, parent.y, child.x, child.y);
+
+		// Calcolatore della percentuale del volume
+		let distance = dist(child.x, child.y, width / 2, height / 2)
+		if (distance < volRadius) {
+			vol = (index - 1) * 10;
+			hitCheck = true;
+		}
+		parent = child;
+		index--;
+	}
+	let distance = dist(mouseX, mouseY, width / 2, height / 2)
+	if (distance < volRadius) {
+		vol = 100;
+		hitCheck = true;
+	}
+
+	if (!hitCheck) {
+		vol = 0;		
+	}
+	console.log(vol);
+
+	// Linee
+	//push();
+		//stroke(255, 50);
+		//strokeWeight(1);
+		//line(0, (height / 2) + 15, windowWidth, (height / 2) + 15);
+		//line(0, (height / 2) - 15, windowWidth, (height / 2) - 15);
+	//pop();
+
+	// Cerchio
+	push();
+	//fill("#FF3838");
+	fill("#FF4122");
+	noStroke();
+	circle(width / 2, height / 2, volRadius * 2);
+	pop();
+
+	// Icona e collegamento alla fine della catena
+	push();
+		tint(255, 220);
+		imageMode(CENTER)
+		translate(pos[pos.length - 1].x, pos[pos.length - 1].y )
+		rotate(angle+PI)
+		translate(-60,0)
+		image(img, 0,0, img.width * 0.6, img.height * 0.6);
+	pop()
+	
+	// Testo diviso
+	push()
+		noStroke();
+		fill(255);
+		textFont(font);
+		textSize(20);
+		text("VOLUME:", windowWidth/2-240, windowHeight/2+160)
+		text(vol, windowWidth/2-160, windowHeight/2+160);
+	pop();
+	
+}
+
+function mousePressed() {
+  mySound.play();
+}
